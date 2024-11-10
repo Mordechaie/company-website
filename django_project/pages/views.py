a315bd06be347c93fdc8e53150e394e26d2ab09e
Mordechaie <mordynys@gmail.com>
2024-11-10 00:09:47 -0500

    about page updated

--- django_project/pages/views.py
+++ django_project/pages/views.py
@@ -1,4 +1,6 @@
+from typing import Any
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
 def home_page_view(request):
@@ -8,3 +10,11 @@ def home_page_view(request):
     }
     return render(request, "home.html", context)
 
+class AboutPageView(TemplateView):
+    template_name = "about.html"
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["contact_address"] = "123 Main Street"
+        context["phone_number"] = "555-555-5555"
+        return context
